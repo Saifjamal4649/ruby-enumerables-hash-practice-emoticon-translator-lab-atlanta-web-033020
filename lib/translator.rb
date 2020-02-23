b4033928d9f0ff01480ab14218f895aa7769eeee
Saif Jamal <saifjamal4649@gmail.com>
2020-02-23 01:50:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@
 require 'yaml'
 
 def load_library(emoticon_file)
-  # code goes here
+  
   emoticons = YAML.load_file('./lib/emoticons.yml')
 
   emoticon_lib = {'get_meaning'  => {},
@@ -17,10 +17,10 @@ def load_library(emoticon_file)
   emoticon_lib
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticon_file, yaml)
+  
 end
 
 def get_english_meaning
-  # code goes here
+ 
 end
